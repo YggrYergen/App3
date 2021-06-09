34c427b9557ef449967a97cd5aa2b9009f248015
Rodrigo Rojas <rod.rojas@alumnos.uai.cl>
2021-06-09 14:04:48 -0400

    Entrada y Salida ubicados

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import sys
 import numpy as np
+from aux import findFirst, findLast
 
 
-def paths(maze):
+def positions(maze):
     return np.argwhere(maze == 0)
 
 
+""" def paths(maze):
+    return findFirst(positions(maze)), findLast(positions(maze))
+ """
+
 if __name__ == "__main__":
     maze = np.loadtxt(sys.argv[1], dtype='i')
-    print(paths(maze))
+    print(findFirst(positions(maze)), findLast(positions(maze)))
+    # print(paths(maze))
